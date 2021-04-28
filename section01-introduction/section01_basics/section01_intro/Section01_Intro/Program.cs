cb88fc7c8dc792a47108364ca25bda28d54700cb
marcoschultznunes <marco_sn@hotmail.com>
2021-04-28 19:57:56 -0300

    C# basics section 01 - intro

---
+++ section01-introduction/section01_basics/section01_intro/Section01_Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Section01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There!");
+        }
+    }
+}
